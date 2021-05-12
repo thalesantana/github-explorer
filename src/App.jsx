b997cfe088fbcea5afbb4be0dbcd9da2de005433
Thales <thales.c.santana@outlook.com>
2021-05-11 21:21:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+
+export function App(){
+    throw new Error('Deu ruim viado');
+    return <h1>Hello World</h1>
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path:path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.jsx', '.js'],
+    },
+    devServer:{
+        contentBase:path.resolve(__dirname, 'public','index.html')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template:path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }
+        ],
+    }
+}
